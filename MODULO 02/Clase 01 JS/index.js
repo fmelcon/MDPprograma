8b36d74b505e5a2a97b20a0ffe62d395b167bbc5
Franco <fmelcon@live.com>
2023-07-17 20:08:52 -0300

    first commit

---
+++ MODULO 02/Clase 01 JS/index.js
@@ -0,0 +1,101 @@
+//////* TIPO DE DATOS JS*/////
+// Valor numerico => NUMBER PRIMITIVE
+// Valor numerico => BIGINT o BIGINTEGER
+// Valor de texto o cadena => STRING PRIMITIVE
+// valor boolean => BOOLEAN (true|false)
+// valor (sin definir) undefined => UNDEFINED 
+// tipo de funcion => FUNCTION()
+// valores en objeto => OBJECT
+// valores (nulo) null => NULL 
+
+////////////////////* VAIABLES *///////////////////////
+////// 3 maneras de declarar una variable en JS ///////
+
+// variable
+var uno = 12;
+
+// variable con bloque {}
+
+let dos = 'Hola';
+
+// variable constante
+
+const tres = '3';
+const cuatro = true; 
+
+//typeof
+
+console.log(typeof uno);
+console.log(typeof dos);
+console.log(typeof tres);
+console.log(typeof cuatro);
+
+////////////////////* OPERADORES *///////////////////////
+
+// Operadores aritmeticos
+
+// Suma                   +
+// resta                  -
+// multiplicacion         *
+// division               /
+// modulo de una division %
+// exponente              **
+
+let x = 34;
+
+console.log(x - uno);
+
+///////////////* OPERADORES DE ASIGNACION *///////////////
+
+// Asignacion             = 
+// sumar y asignar       +=
+// restar y asignar      -=
+// dividir y asignar     /=
+// multiplicar y asignar *=
+// modulo y asignar      %=
+// exponente y asignar  **=
+
+// Asignacion uno = uno - x
+console.log(uno -= x);
+
+//////////////////* OPERADORES UNARIOS *///////////////////
+
+// Operadores unarios
+
+// decremento    variable --
+// incremento    variable ++
+// incremento previo ++variable
+// decremento previo --variable
+
+/////////////////* Operadores comparativo*////////////////
+
+// Operadores comparativos
+
+// igualdad               ==
+// igualdad de identidad === 
+// diferencia             !=
+// diferencia de identidad!=
+// menor que               <
+// mayor que               >
+// menor e igual          <=
+// mayor o igual          >=
+
+
+////////////////* OPERADORES BINARIOS *///////////////////
+
+// Operadores binarios
+
+// OR                   |
+// AND                  &
+
+
+///////////////* OPERADORES LOGICOS */////////////////////
+
+// Operadores logicos
+
+// OR                   ||
+// AND                  &&
+// NOT                   !
+// operador ternario    ?:
+// operador opcional     ?
+
