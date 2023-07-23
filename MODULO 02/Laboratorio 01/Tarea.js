d71197861aad06ac46ab9ff551434ee10936d0a1
Franco <fmelcon@live.com>
2023-07-23 12:02:32 -0300

    first exercise

--- MODULO 02/Laboratorio 01/Tarea.js
+++ MODULO 02/Laboratorio 01/Tarea.js
@@ -3,3 +3,24 @@
 *Realizar la tabla de multiplicar dado un valor por el usuario
 *Realizar la tabla de dividir dado un valor ingresado por el usuario
 */
+
+const prompt = require('prompt-sync')({signit:true});
+//paqueteria para leer el prompt desde consola...
+
+
+let a = prompt('Ingrese el valor de a: ');
+let b= prompt('Ingrese el valor de b: ');
+
+//con IF normal 
+if (a > b ) {
+    mayor = a;
+    } else {
+    mayor = b;
+    } 
+    
+//console.log('El numero mayor es:',mayor);
+// ternario
+
+mayor = (a > b) ? console.log(`El numero mayor es ${a}`) : console.log(`El numero mayor es ${b}`);
+
+
