bf28cb635488c658320a7ab42cd2f8c835cdd0db
Franco <fmelcon@live.com>
2023-07-24 18:05:17 -0300

    Homework

--- MODULO 02/Laboratorio 01/Tarea.js
+++ MODULO 02/Laboratorio 01/Tarea.js
@@ -1,26 +1,53 @@
-/*
-*DADO 3 valores ingresados por el usuario saber cual es el mayor de ellos.
-*Realizar la tabla de multiplicar dado un valor por el usuario
-*Realizar la tabla de dividir dado un valor ingresado por el usuario
-*/
+//////////////////////TAREA////////////////////
 
 const prompt = require('prompt-sync')({signit:true});
 //paqueteria para leer el prompt desde consola...
 
 
+//DADO 3 valores ingresados por el usuario saber cual es el mayor de ellos.
+
 let a = prompt('Ingrese el valor de a: ');
 let b= prompt('Ingrese el valor de b: ');
+let c= prompt('Ingrese el valor de c: ');
+
+let mayor = a
 
 //con IF normal 
-if (a > b ) {
-    mayor = a;
-    } else {
+if (b > mayor && b > c) {
     mayor = b;
-    } 
-    
-//console.log('El numero mayor es:',mayor);
+}else if (c > mayor && c > b && c > a) {
+    mayor = c;
+}
+
+console.log('El numero mayor es:',mayor);
+
 // ternario
 
-mayor = (a > b) ? console.log(`El numero mayor es ${a}`) : console.log(`El numero mayor es ${b}`);
+/*mayor = (a > b && a > c) ? console.log(`El numero mayor es ${a}`):
+        (b > a && b > c) ? console.log(`El numero mayor es ${b}`):
+        console.log(`El numero mayor es ${c}`);*/
 
 
+///////////////////////////////////////////////////////////////////////////////
+//Realizar la tabla de multiplicar dado un valor por el usuario
+
+let multiplo = prompt('Ingrese un numero para hacer tabla de multiplicar: ');
+console.log(`Tabla de multiplicar del numero: ${multiplo}`);
+
+for (let i = 1; i <= 10; i++) {
+    const resultado = multiplo * i;
+
+    console.log(`${multiplo} x ${i} = ${resultado}`);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+let divisor = prompt('Ingrese un numero para hacer tabla de dividir :');
+console.log(`Tabla de multiplicar del numero: ${divisor}`);
+
+for (let i = 1; i <= 10; i++) {
+    const resultado = divisor / i;
+    const numerosindecimales = resultado.toFixed(2);
+    console.log(`${divisor} / ${i} = ${parseFloat(numerosindecimales)}`);
+}
